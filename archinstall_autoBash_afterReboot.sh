ab4689f8270244834d9faf58c1ebb1d535b7228e
sanTmue <10733637-sanmue@users-noreply.gitlab.com>
2024-11-20 20:10:33 +0100

    modify: System on ESP: added grml; refactor code to fit for archiso and grml

--- archinstall_autoBash_afterReboot.sh
+++ archinstall_autoBash_afterReboot.sh
@@ -96,34 +96,47 @@ fi
 
 
 # ### --------------------------
-# Archiso on ESP (rescue system)
+# System on ESP (rescue system)
+# - https://wiki.archlinux.org/title/Systemd-boot#Grml_on_ESP
 # - https://wiki.archlinux.org/title/Systemd-boot#Archiso_on_ESP
-if [ "${archisoOnESP}" = "true" ] && [ "${bootloader}" = "systemd-boot" ]; then
+if [ "${bootloader}" = "systemd-boot" ]; then
     downloadFolder="${HOME}/Downloads"
-    isoFileName=""
-    searchPattern="archlinux*.iso"
-    archiso=""
 
-    echo -e "\e[0;35m## Archiso on ESP\e[39m"
-    echo "Please make sure you downloaded the archlinux installation iso to '${downloadFolder}'"
-    # read -r -p "Press enter to continue"
+    echo -e "\e[0;35m## System on ESP\e[39m"
+    echo "Please make sure you downloaded the installation iso to '${downloadFolder}'"
 
-    # archlinux-YYYY.MM.DD-x86_64.iso, e.g.: /home/userid/Downloads/archlinux-2024.11.01-x86_64.iso
-    isoFileName=$(find "${downloadFolder}" -mindepth 1 -maxdepth 1 -prune -type f -name "${searchPattern}" | cut -d'/' -f5)
-    if [ -z "${isoFileName}" ]; then
-        echo -e "\e[0;31mNo archlinix installation iso found, skipping step 'Archiso on ESP'...\e[39m"
-        echo "You can download the iso to '${downloadFolder}' and start this script again later."
-    else
-        echo "- found iso-file: '${isoFileName}' in '${downloadFolder}'"
-        # isoFileName=$(ls "${downloadFolder}" | grep arch\.\*.iso\$)
-        # shopt -s extglob
-        # isoFileName=$(ls -- "${downloadFolder}/arch*.iso" | cut -d'/' -f5)
+    for system in "${systemOnESP[@]}"; do
+        isoFileName=""
+        iso=""
 
-        archiso="${downloadFolder}/${isoFileName}"
-        config-systemOnEsp "${archiso}" "archiso" # P1: path to archiso # P2: folder name on efi partition for system
-    fi
+        if [ "${system}" = "archiso" ]; then
+            searchPattern="archlinux*.iso"
+        elif [ "${system}" = "grml" ]; then
+            searchPattern="grml64-full*.iso"
+        else
+            continue
+        fi
+
+        # e.g.: archlinux-YYYY.MM.DD-x86_64.iso # grml64-FLAVOUR_YEAR.MONTH.iso
+        # e.g.: /home/userid/Downloads/archlinux-2024.11.01-x86_64.iso # /home/userid/Downloads/grml64-full_2024.02.iso
+        isoFileName=$(find "${downloadFolder}" -mindepth 1 -maxdepth 1 -prune -type f -name "${searchPattern}" | cut -d'/' -f5)
+        if [ -z "${isoFileName}" ]; then
+            echo -e "\e[0;31mNo installation iso found. Skipping 'System on ESP' for '${system}'\e[39m"
+            echo "You can download the iso to '${downloadFolder}' and start this script again later."
+        else
+            echo "- found iso-file: '${isoFileName}' in '${downloadFolder}'"
+            # isoFileName=$(ls "${downloadFolder}" | grep arch\.\*.iso\$)
+            # shopt -s extglob
+            # isoFileName=$(ls -- "${downloadFolder}/arch*.iso" | cut -d'/' -f5)
+
+            iso="${downloadFolder}/${isoFileName}"
+            config-systemOnEsp "${iso}" "${system}" "${isoFileName}"
+            fi
+    done
+else
+    echo "Skipping 'System on ESP (rescue system)'. Currently only available for systemd-boot."
 fi
-# ### END Archiso on ESP -------
+# ### END System on ESP -------
 
 
 # --- Info message:
